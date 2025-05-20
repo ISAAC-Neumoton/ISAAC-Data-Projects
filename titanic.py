46a50c84ec57cba1a51468c96692dc1864a130c6
Isaac <isaacujay@mail.com>
2025-05-20 17:28:13 +0100

    Titanic data cleaned with missing data heatmap graph

--- titanic.py
+++ titanic.py
@@ -1,9 +1,35 @@
 import pandas as pd
-
+import seaborn as sb
+import matplotlib.pyplot as plot
 
 data = pd.read_csv("tested.csv")
 print(data.head())
 # print(data.info())
 # print(data.describe())
 # print(data.isnull().sum())
-print(data['Cabin'].value_counts())
+
+# Day 2: Visualization  of Missing Data
+# define the figure size
+plot.figure(figsize = (10, 8))
+# plot the heatmap
+sb.heatmap(data.isnull(), cbar = False,  cmap = "viridis")
+# show the plot TITLE
+plot.title("Missing Data in Titanic Dataset")
+# show the plot
+# plot.show()
+
+
+#  remove the age colomn
+data = data.drop(columns = ['Age'])
+
+# replace the missing value in Fare with mean
+data['Fare'] = data['Fare'].fillna(data['Fare'].mean())
+
+# show only the first letter of the cabin
+data['Cabin'] = data['Cabin'].str[0]
+# relace the missing values with 'U' for unknown
+data['Cabin'] = data['Cabin'].fillna('U')
+# show the first 5 rows of the Cabin column
+print(data.isnull().sum())
+
+print(data.head())
